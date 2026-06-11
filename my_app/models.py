9848e589ad38d6901f6aa0a03b5215e10a733c54
Michelle Cheng <cheng.michelley@gmail.com>
2026-06-10 22:03:20 -0400

    Added Project Model

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+# Users
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Projects
+class Project(models.Model):
+    STATUS_CHOICES = [
+        ("planning", "Planning"),
+        ("active", "Active"),
+        ("completed", "Completed"),
+        ("archived", "Archived"),
+    ]
+    
+    name = models.CharField(max_length= 200)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length = 20,
+        choices= STATUS_CHOICES,
+        default = "planning"
+    )
+    
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return self.name
+    
+# Experiments
+
+# Samples
+
+# Tasks
+
+# Quality Control Review
+
+# Audit Logs
